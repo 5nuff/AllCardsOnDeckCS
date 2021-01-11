acab59c98dfa20bf5d86df6468a2a9facf0536f4
Nathan <intechnic.l.r@gmail.com>
2021-01-11 14:32:22 -0500

    cards displaying, shuffle incomplete

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            var suitValues = suits.Count;
+            var rankValues = ranks.Count;
+
+            foreach (var chosenSuit in suits)
+            {
+                foreach (var chosenRank in ranks)
+                {
+                    var card = chosenSuit + chosenRank;
+                    Console.WriteLine(card);
+                }
+            }
+        }
     }
-  }
 }
